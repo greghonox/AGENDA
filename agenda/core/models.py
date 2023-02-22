fa4da59dff8dfe297e6acd47aaf5332954cece09
Gregorio Honorato <greghono@gmail.com>
2023-02-22 20:07:27 +0000

    feat: adding models for django admin

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,44 +1,55 @@
 from django.db import models
 
 special_choice = (
-    (0, 'Cirurgião plástico'),
-    (1, 'Psiquiatra'),
-    (2, 'Pediatra'),
-    (3, 'Neurologista'),
-    (4, 'Oncologista'),
-    (5, 'Ginecologista'),
-    (6, 'Homeopata'),
-    (7, 'Alergista'),
-    (8, 'Nutricionista'),
-    (9, 'Pneumologista'),
-    (10, 'Dermatologista'),
-    (11, 'Geriatra'),
-    (12, 'Neurocirurgião'),
-    (13, 'Angiologista'),
-    (14, 'Urologista'),
-    (15, 'Psicólogo'),
-    (16, 'Cardiologista'),
-    (17, 'Acupunturista'),
-    (18, 'Neuropsicólogo'),
-    (19, 'RPG'),
-    (20, 'Ortopedista e Traumatologista'),
+    ('cp', 'Cirurgião plástico'),
+    ('ps', 'Psiquiatra'),
+    ('pd', 'Pediatra'),
+    ('ne', 'Neurologista'),
+    ('on', 'Oncologista'),
+    ('gi', 'Ginecologista'),
+    ('ho', 'Homeopata'),
+    ('al', 'Alergista'),
+    ('nu', 'Nutricionista'),
+    ('pn', 'Pneumologista'),
+    ('de', 'Dermatologista'),
+    ('ge', 'Geriatra'),
+    ('nr', 'Neurocirurgião'),
+    ('an', 'Angiologista'),
+    ('ur', 'Urologista'),
+    ('pi', 'Psicólogo'),
+    ('ca', 'Cardiologista'),
+    ('ac', 'Acupunturista'),
+    ('nu', 'Neuropsicólogo'),
+    ('rp', 'RPG'),
+    ('ot', 'Ortopedista e Traumatologista'),
 )
 
 class Doctor(models.Model):
-    full_name = models.CharField(max_length=255, verbose_name='Nome Completo', null=True)
-    observation = models.CharField(max_length=255, verbose_name='Observação', null=False)
-    special = models.CharField(choices=special_choice, verbose_name='Especialidade médica', null=True)
+    full_name = models.CharField(max_length=255, verbose_name='Nome Completo', blank=False, error_messages={'required': 'Por favor insira um nome valido'})
+    observation = models.CharField(max_length=255, verbose_name='Observação', blank=True, default='')
+    special = models.CharField(max_length=50, choices=special_choice, verbose_name='Especialidade médica', blank=False)
+    
+    def __str__(self):
+        return f'{self.full_name} {self.special}'
 
 class Patient(models.Model):
-    full_name = models.CharField(max_length=255, verbose_name='Nome Completo', null=True)
-    observation = models.CharField(max_length=255, verbose_name='Observação', null=False)
+    full_name = models.CharField(max_length=255, verbose_name='Nome Completo', blank=False, error_messages={'required': 'Por favor insira um nome valido'})
+    observation = models.CharField(max_length=255, verbose_name='Observação', blank=True, default='')
+    
+    def __str__(self):
+        return f'{self.full_name}'
     
 
 class Schedule(models.Model):
-    doctor = models.ForeignKey(Doctor, verbose_name='Doutor', on_delete=models.CASCADE, null=True)
-    patient = models.ForeignKey(Patient, verbose_name='Paciente', on_delete=models.CASCADE, null=True)
-    date = models.DateTimeField(verbose_name='Data e Hora', null=True)
+    doctor = models.ForeignKey(Doctor, verbose_name='Doutor', on_delete=models.CASCADE, blank=False)
+    patient = models.ForeignKey(Patient, verbose_name='Paciente', on_delete=models.CASCADE, blank=False)
+    date = models.DateTimeField(verbose_name='Data e Hora', blank=False)
     
 
     class Meta:
-        unique_together = (('Doctor', 'Patient', 'date'),)
+        unique_together = (('doctor', 'patient', 'date'),)
+        
+
+    def __str__(self):
+        return f'{self.doctor} {self.patient} {self.date}'
+            
---
+++ agenda/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+import datetime
+from django.forms import ValidationError
+from core.models import Schedule
+
+
+class ScheduleForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = ['doctor', 'patient', 'date'] 
+    
+
+    def clean(self):
+        doctor = self.cleaned_data.get('doctor')
+        patient = self.cleaned_data.get('patient')
+        date = self.cleaned_data.get('date')
+        if datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) > date:
+            raise forms.ValidationError('Não pode fazer agendamento antes da data corrente!')
+        if len(self.get_schedule_hour(doctor, patient, date)) > 0:
+            raise forms.ValidationError(f'O agendamento para {patient} com o doutor: {doctor} já existe na data: {date}')
+        
+    def get_schedule_hour(self, doctor, patient, date):
+        data_start = datetime.datetime(date.year, date.month, date.day, date.hour, 0)
+        data_end = datetime.datetime(date.year, date.month, date.day, date.hour, 59)
+        return Schedule.objects.filter(date__range=(data_start, data_end), doctor=doctor, patient=patient)
